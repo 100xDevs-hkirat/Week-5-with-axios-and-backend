5728fa5c9df016e68829801aea373297a7c73daa
Kirat <harkirat96@gmail.com>
2023-08-06 21:03:03 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ app.use(express.json());
 
 app.use("/admin", adminRouter)
 app.use("/user", userRouter)
-app.get("/", (req, res) => res.json({msg: "hello world22"}));
+app.get("/", (req, res) => res.json({msg: "hello world after the class"}));
 
 // Connect to MongoDB
 // DONT MISUSE THIS THANKYOU!!
